fc0291ee76f80ba0d35136b5fd5e9a0699bfa251
JackeyLee <447881262@qq.com>
2020-10-11 07:37:55 +0800

    push test01

--- src/main/java/com/jackeylee/git/Demo.java
+++ src/main/java/com/jackeylee/git/Demo.java
@@ -7,6 +7,6 @@ package com.jackeylee.git;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld ");
     }
 }
