6cc16d3e31850db9b3f951b228aad4903e70fbfc
lisi <lisi@lisi.com>
2020-10-31 00:29:08 +0800

    "李四插入了1行代码2020年10月31日00:27:38" "李四插入了第2行代码2020年10月31日00:27:55"

--- src/main/java/com/jackeylee/git/Demo.java
+++ src/main/java/com/jackeylee/git/Demo.java
@@ -13,6 +13,7 @@ public class Demo {
         System.out.println("改变了一下" + new Date());
         System.out.println("创建了dev分支");
         System.out.println("创建了feature分支，未来可以与master分支合并");
-
+        System.out.println("李四插入了1行代码2020年10月31日00:27:38");
+        System.out.println("李四插入了第2行代码2020年10月31日00:27:55");
     }
 }
