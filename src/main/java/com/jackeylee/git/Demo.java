497004b95d3bf1d725db442e9ff6c82d9352375d
l84189669 <liguodong30@huawei.com>
2021-06-06 07:27:32 +0800

    6.6日测试提交github

--- src/main/java/com/jackeylee/git/Demo.java
+++ src/main/java/com/jackeylee/git/Demo.java
@@ -11,7 +11,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("HelloWorld ");
         System.out.println("改变了一下" + new Date());
-        System.out.println("创建了dev分支");
-
+        System.out.println("master改变了一下,2020年10月31日00:10:34");
+        System.out.println("6.6日更新");
     }
 }
